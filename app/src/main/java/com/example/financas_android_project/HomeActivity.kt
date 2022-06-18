0fda91cc054c5eb009099525a61b9cd5a2531b9b
Pedro Henrique Galvão <70819419+pedrohenriquegalvao@users.noreply.github.com>
2022-06-18 13:17:20 -0300

    Incrementando DespesasAdapter e iniciando na Home page

--- app/src/main/java/com/example/financas_android_project/HomeActivity.kt
+++ app/src/main/java/com/example/financas_android_project/HomeActivity.kt
@@ -3,19 +3,38 @@ package com.example.financas_android_project
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.financas_android_project.adapter.DespesasAdapter
+import com.example.financas_android_project.database.DatabaseHelper
+import com.example.financas_android_project.model.DespesaModel
 
 class HomeActivity : AppCompatActivity() {
+
+    lateinit var recycler_despesa : RecyclerView
+    lateinit var btn_add : Button
+    var despesasAdapter : DespesasAdapter ?= null
+    var bancoDeDados : DatabaseHelper ?= null
+    var despesas : List<DespesaModel> = ArrayList<DespesaModel>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
-        // Get the Intent that started this activity and extract the string
         val message = intent.getStringExtra(EXTRA_MESSAGE)
 
-        // Capture the layout's TextView and set the string as its text
         val textView = findViewById<TextView>(R.id.tituloHome).apply {
             text = "Bem vindo $message"
+
         }
+        recycler_despesa = findViewById(R.id.recyclerViewDespesas)
+        btn_add = findViewById(R.id.buttonAddDespesa)
+
+        bancoDeDados = DatabaseHelper(this)
+
+
     }
+
+
 }
--- app/src/main/java/com/example/financas_android_project/adapter/DespesasAdapter.kt
+++ app/src/main/java/com/example/financas_android_project/adapter/DespesasAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.financas_android_project.adapter
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.financas_android_project.AddDespesa
 import com.example.financas_android_project.R
 import com.example.financas_android_project.model.DespesaModel
 import org.w3c.dom.Text
@@ -33,7 +35,18 @@ class DespesasAdapter(despesas: List<DespesaModel>, internal var context: Contex
     }
 
     override fun onBindViewHolder(holder: DespesaViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val despesa = despesas[position]
+        holder.nome_despesa.text = despesa.nome_despesa
+        holder.data_venc.text = despesa.data_venc
+        holder.valor.text = despesa.valor.toString()
+
+        holder.btn_edit.setOnClickListener {
+            val i = Intent(context, AddDespesa::class.java)
+            i.putExtra("Modo", "Editar")
+            i.putExtra("Id", despesa.id_despesa)
+            context.startActivity(i)
+
+        }
     }
 
     override fun getItemCount(): Int {
